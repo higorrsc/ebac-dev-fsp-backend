90cb5c4fb54bf43fc9df833c182a704192c2378a
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-08-24 18:26:23 -0300

    alteração para exercício

--- algorithms/bubble_sort.py
+++
@@ -1,7 +0,0 @@
-def bubble_sort(unsorted_list):
-    for i in range(len(unsorted_list) - 1):
-        for j in range(len(unsorted_list) - 1 - i):
-            if unsorted_list[j] > unsorted_list[j + 1]:
-                unsorted_list[j], unsorted_list[j +
-                                                1] = unsorted_list[j + 1], unsorted_list[j]
-    return unsorted_list
--- tdd/math_operations.py
+++
@@ -1,6 +0,0 @@
-def addition_operation(num_1, num_2):
-    return num_1 + num_2
-
-
-def subtraction_operation(num_1, num_2):
-    return num_1 - num_2
